9b698ea1ead1c66eb651523eaffd8f416fe219a8
youssefhany2005 <yossefhany0@gmail.com>
2025-08-02 21:55:17 +0300

    Full Demo

---
+++ Demo03OOP/Parent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo03OOP
+{
+    internal class Parent
+    {
+        #region Properties
+        public int X { get; set; }
+        public int Y { get; set; }
+        #endregion
+
+        #region Constructor
+        public Parent(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        #endregion
+
+        #region Methods
+        public virtual int Product()
+        {
+            return X * Y;
+        }
+
+
+        public override string ToString()
+        {
+            return $"X = {X}, Y = {Y}";
+        }
+
+
+        public void MyFun()
+        {
+            Console.WriteLine("Iam Parent");
+        }
+        #endregion
+    }
+}
